ed418f24c8c7e3f8cd2e635c7d3c0b99a046a994
Selim <selimchemutai19@gmail.com>
2021-11-06 08:48:32 +0300

    Create main.c

---
+++ Watch-Code/Watch-Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * Watch-Code.c
+ *
+ * Created: 06/11/2021 08:47:50
+ * Author : Default
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
